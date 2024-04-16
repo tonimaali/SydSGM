74a22a83784ea95ea09685047cb6a19b01deaa09
tonimaali <73574216+tonimaali@users.noreply.github.com>
2024-04-16 17:56:51 +1000

    Create TA_get_tckmap_vol_50subs_4mask.m

---
+++ TA_get_tckmap_vol_50subs_4mask.m
@@ -0,0 +1,153 @@
+function [matrix_tckmap_vol_data_all_subs] = TA_get_tckmap_vol_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum)
+
+% sub1
+matrix_tckmap_vol_warped(:, 1:10) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub1);
+
+% sub2
+matrix_tckmap_vol_warped(:, 11:20) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub2);
+
+% sub3
+matrix_tckmap_vol_warped(:, 21:30) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub3);
+
+% sub4
+matrix_tckmap_vol_warped(:, 31:40) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub4);
+
+% sub5
+matrix_tckmap_vol_warped(:, 41:50) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub5);
+
+% sub6
+matrix_tckmap_vol_warped(:, 51:60) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub6);
+
+% sub7
+matrix_tckmap_vol_warped(:, 61:70) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub7);
+
+% sub8
+matrix_tckmap_vol_warped(:, 71:80) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub8);
+
+% sub9
+matrix_tckmap_vol_warped(:, 81:90) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub9);
+
+% sub10
+matrix_tckmap_vol_warped(:, 91:100) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub10);
+
+% sub11
+matrix_tckmap_vol_warped(:, 101:110) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub11);
+
+% sub12
+matrix_tckmap_vol_warped(:, 111:120) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub12);
+
+% sub13
+matrix_tckmap_vol_warped(:, 121:130) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub13);
+
+% sub14
+matrix_tckmap_vol_warped(:, 131:140) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub14);
+
+% sub15
+matrix_tckmap_vol_warped(:, 141:150) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub15);
+
+% sub16
+matrix_tckmap_vol_warped(:, 151:160) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub16);
+
+% sub17
+matrix_tckmap_vol_warped(:, 161:170) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub17);
+
+% sub18
+matrix_tckmap_vol_warped(:, 171:180) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub18);
+
+% sub19
+matrix_tckmap_vol_warped(:, 181:190) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub19);
+
+% sub20
+matrix_tckmap_vol_warped(:, 191:200) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub20);
+
+% sub21
+matrix_tckmap_vol_warped(:, 201:210) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub21);
+
+% sub22
+matrix_tckmap_vol_warped(:, 211:220) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub22);
+
+% sub23
+matrix_tckmap_vol_warped(:, 221:230) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub23);
+
+% sub24
+matrix_tckmap_vol_warped(:, 231:240) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub24);
+
+% sub25
+matrix_tckmap_vol_warped(:, 241:250) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub25);
+
+% sub26
+matrix_tckmap_vol_warped(:, 251:260) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub26);
+
+% sub27
+matrix_tckmap_vol_warped(:, 261:270) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub27);
+
+% sub28
+matrix_tckmap_vol_warped(:, 271:280) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub28);
+
+% sub29
+matrix_tckmap_vol_warped(:, 281:290) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub29);
+
+% sub30
+matrix_tckmap_vol_warped(:, 291:300) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub30);
+
+% sub31
+matrix_tckmap_vol_warped(:, 301:310) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub31);
+
+% sub32
+matrix_tckmap_vol_warped(:, 311:320) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub32);
+
+% sub33
+matrix_tckmap_vol_warped(:, 321:330) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub33);
+
+% sub34
+matrix_tckmap_vol_warped(:, 331:340) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub34);
+
+% sub35
+matrix_tckmap_vol_warped(:, 341:350) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub35);
+
+% sub36
+matrix_tckmap_vol_warped(:, 351:360) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub36);
+
+% sub37
+matrix_tckmap_vol_warped(:, 361:370) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub37);
+
+% sub38
+matrix_tckmap_vol_warped(:, 371:380) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub38);
+
+% sub39
+matrix_tckmap_vol_warped(:, 381:390) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub39);
+
+% sub40
+matrix_tckmap_vol_warped(:, 391:400) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub40);
+
+% sub41
+matrix_tckmap_vol_warped(:, 401:410) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub41);
+
+% sub42
+matrix_tckmap_vol_warped(:, 411:420) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub42);
+
+% sub43
+matrix_tckmap_vol_warped(:, 421:430) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub43);
+
+% sub44
+matrix_tckmap_vol_warped(:, 431:440) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub44);
+
+% sub45
+matrix_tckmap_vol_warped(:, 441:450) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub45);
+
+% sub46
+matrix_tckmap_vol_warped(:, 451:460) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub46);
+
+% sub47
+matrix_tckmap_vol_warped(:, 461:470) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub47);
+
+% sub48
+matrix_tckmap_vol_warped(:, 471:480) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub48);
+
+% sub49
+matrix_tckmap_vol_warped(:, 481:490) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub49);
+
+% sub50
+matrix_tckmap_vol_warped(:, 491:500) = TA_read_tckmap_vol_warped_50subs_4mask(mask, fwhm, loc_warped_data, loc_data_50sub, Subnum.sub50);
+
+matrix_tckmap_vol_data_all_subs = matrix_tckmap_vol_warped;
